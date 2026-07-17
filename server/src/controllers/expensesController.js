c7b369f39824c2418dcebacf044edef05db70a46
Danyal Saeed <cherrylotter@gmail.com>
2026-07-16 20:37:55 -0400

    Merged backend with postgres

--- server/src/controllers/expensesController.js
+++ server/src/controllers/expensesController.js
@@ -1,27 +1,64 @@
 
 
-let expenses = [
-    { id: 1, description: 'Groceries', amount: 42.5, category: 'Food', date: '2026-07-11' },
-];
-let nextId = 2;
+// let expenses = [
+//     { id: 1, description: 'Groceries', amount: 42.5, category: 'Food', date: '2026-07-11' },
+// ];
+// let nextId = 2;
 
-function getAllExpenses(req, res){
-    res.json(expenses);
+// function getAllExpenses(req, res){
+//     res.json(expenses);
+// }
+
+// function createExpense(req, res){
+//     const { description, amount, category, date } = req.body;
+//     if (!description || amount === undefined) {
+//         return res.status(400).json({ error: 'Description and amount are required' });
+//     }
+
+//     const expense = { id: nextId++, description, amount, category, date };
+//     expenses.push(expense);
+//     res.status(201).json(expense);
+// }
+
+// module.exports = {
+//     getAllExpenses,
+//     createExpense
+// };
+
+const pool = require('../db');
+
+async function getAllExpenses(req, res){
+    try {
+        const result = await pool.query('SELECT * FROM expenses');
+        res.json(result.rows);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: "Internal server error"});
+    }
+
+
 }
 
-function createExpense(req, res){
+async function createExpense(req, res){
     const { description, amount, category, date } = req.body;
     if (!description || amount === undefined) {
         return res.status(400).json({ error: 'Description and amount are required' });
     }
 
-    const expense = { id: nextId++, description, amount, category, date };
-    expenses.push(expense);
-    res.status(201).json(expense);
+    try {
+        const result = await pool.query(
+            `INSERT INTO expenses (description, amount, category, date) 
+            VALUES ($1, $2, $3, COALESCE($4::date, CURRENT_DATE))
+            RETURNING *`, 
+        [description, amount, category, date]
+        );
+        res.status(201).json(result.rows[0]);
+    }catch (err) {
+        console.log(err);
+        res.status(501).json({error: 'Internal Server Error'});
+    }
+
+
 }
 
-module.exports = {
-    getAllExpenses,
-    createExpense
-};
-
+module.exports = { getAllExpenses, createExpense};
